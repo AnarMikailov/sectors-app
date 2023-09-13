817faa2d4e1917c9fc18d2968ac3eabe9e6ec39e
AnarMikailov <a.v.mikayilov@gmail.com>
2023-09-13 22:34:33 +0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
 import React, { useEffect, useState } from "react";
-
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 function App() {
-  const [data, setData] = useState(null);
-
+  const [sectors, setSectors] = useState([]);
+  const sectorCollectionsRef = collection(db, "Sectors");
   useEffect(() => {
-    // Define the URL of the Firebase Realtime Database
-    const apiUrl = "https://spa-choosing-sectors-default-rtdb.firebaseio.com/";
-
-    // Fetch data from the Firebase Realtime Database
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setData(data); // Set the fetched data to the state
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    const getSectors = async () => {
+      const data = await getDocs(sectorCollectionsRef);
+      setSectors(...data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(sectors.sectors);
+    };
+    getSectors();
   }, []);
 
-  return (
-    <div className="App">
-      <h1>Fetched Data from Firebase</h1>
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : (
-        <p>Loading data...</p>
-      )}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,41 +1,16 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyBdkAmnUXLpg519HlhX1u5W7bwlI1XOoQE",
+  authDomain: "spa-choosing-sectors.firebaseapp.com",
+  databaseURL: "https://spa-choosing-sectors-default-rtdb.firebaseio.com",
+  projectId: "spa-choosing-sectors",
+  storageBucket: "spa-choosing-sectors.appspot.com",
+  messagingSenderId: "761903541484",
+  appId: "1:761903541484:web:f6ac08fd814b8a60bbc9c9",
+  measurementId: "G-XEPSJEY99K",
+};
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBdkAmnUXLpg519HlhX1u5W7bwlI1XOoQE",
-//   authDomain: "spa-choosing-sectors.firebaseapp.com",
-//   projectId: "spa-choosing-sectors",
-//   storageBucket: "spa-choosing-sectors.appspot.com",
-//   messagingSenderId: "761903541484",
-//   appId: "1:761903541484:web:f6ac08fd814b8a60bbc9c9",
-//   measurementId: "G-XEPSJEY99K",
-// };
-
-// // Initialize Firebase
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// // Read the JSON file (assuming it's named "data.json" and located in the public folder)
-// import jsonData from "./utils/addFireBase";
-// console.log(jsonData);
-// // Create a reference to the Firestore collection where you want to store the JSON data
-// const sectorsCollectionRef = collection(db, "sectors");
-
-// // Convert the JSON data to a JavaScript object (if it's not already)
-// const dataObject = JSON.parse(jsonData);
-
-// // Add the JavaScript object to the Firestore collection
-// addDoc(sectorsCollectionRef, dataObject)
-//   .then((docRef) => {
-//     console.log("Document written with ID: ", docRef.id);
-//   })
-//   .catch((error) => {
-//     console.error("Error adding document: ", error);
-//   });
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
